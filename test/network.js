ea292ae35f11349ecc7302560c78c211e1a03475
redhoodsu <redhoodsu@tencent.com>
2025-05-07 14:07:06 +0800

    fix: test

--- test/network.js
+++ test/network.js
@@ -7,8 +7,10 @@ describe('network', function () {
     it('xhr', function (done) {
       $('.eruda-clear-xhr').click()
       util.ajax.get(window.location.toString(), function () {
-        expect($('.eruda-requests .luna-data-grid-node')).toHaveLength(1)
-        done()
+        setTimeout(function () {
+          expect($('.eruda-requests .luna-data-grid-node')).toHaveLength(1)
+          done()
+        }, 500)
       })
     })
   })
